73680b5c1615bb74c3d1fc8d33e476bb60f14913
ErdalNayir <19360859049@ogrenci.btu.edu.tr>
2023-04-26 02:56:36 +0300

    Resume section has been added

--- src/data.js
+++ src/data.js
@@ -9,3 +9,46 @@ export const urls = {
   note: "https://sticknoteappbyerdal.netlify.app/",
   mapDet: "https://map-detective.web.app/gamemodpage",
 };
+
+export const imgData = {
+  aspNet: "https://www.natmarchand.fr/wp-content/uploads/2018/05/asp.net_.jpg",
+  python:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1869px-Python-logo-notext.svg.png",
+  javascript:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/2048px-Unofficial_JavaScript_logo_2.svg.png",
+  express:
+    "https://w7.pngwing.com/pngs/925/447/png-transparent-express-js-node-js-javascript-mongodb-node-js-text-trademark-logo.png",
+  nodejs:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/2560px-Node.js_logo.svg.png",
+  flutter:
+    "https://w7.pngwing.com/pngs/537/866/png-transparent-flutter-hd-logo.png",
+  react:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
+  redux: "https://raw.githubusercontent.com/reduxjs/redux/master/logo/logo.png",
+  postman: "https://cdn.worldvectorlogo.com/logos/postman.svg",
+  VisualStudio:
+    "https://1000logos.net/wp-content/uploads/2020/08/Visual-Studio-Logo.png",
+  visualStudioCode:
+    "https://logowik.com/content/uploads/images/visual-studio-code7642.jpg",
+  tensorflow:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Tensorflow_logo.svg/1915px-Tensorflow_logo.svg.png",
+  selenium:
+    "https://upload.wikimedia.org/wikipedia/commons/d/d5/Selenium_Logo.png",
+  excel:
+    "https://p.kindpng.com/picc/s/133-1335572_microsoft-excel-logo-png-transparent-png.png",
+  powerpoint:
+    "https://1000logos.net/wp-content/uploads/2020/08/Microsoft-PowerPoint-Logo.png",
+  bash: "https://banner2.cleanpng.com/20180705/txh/kisspng-bash-shell-script-command-line-interface-z-shell-5b3df571eaf1a4.5375084915307871859623.jpg",
+  linux:
+    "https://logos-world.net/wp-content/uploads/2020/09/Linux-Logo-1996-present.png",
+  php: "https://logowik.com/content/uploads/images/php.jpg",
+  chatGpt: "https://1000logos.net/wp-content/uploads/2023/02/ChatGPT-Logo.jpg",
+  html: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/2048px-HTML5_logo_and_wordmark.svg.png",
+  css: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1200px-CSS3_logo_and_wordmark.svg.png",
+  tailwind:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/1024px-Tailwind_CSS_Logo.svg.png",
+  boostrap:
+    "https://img.freepik.com/premium-vector/bootstrap-icon-b-letter-logo_781017-7.jpg",
+  excelVBA:
+    "https://static-00.iconduck.com/assets.00/file-type-vba-icon-512x286-atniaiws.png",
+};
--- src/modules/resume.js
+++ src/modules/resume.js
@@ -1,10 +1,118 @@
 import React from "react";
 import SideBar from "../components/sideBar";
+import { imgData } from "../data";
 
 function Resume() {
   return (
     <div className="flex flex-row">
       <SideBar></SideBar>
+      <div className="ml-16">
+        <div className="flex flex-row  gap-x-32  lg:transition-shadow  duration-300">
+          <div className="mt-16 ml-4  lg:transition-shadow flex-1 duration-300">
+            <h2 className="text-4xl ">Erdal NAYİR</h2>
+            <p className="font-bold mt-4 mb-2 text-sm">
+              Software Engineer Candidate
+            </p>
+            <p className="w-80">
+              I am 3rd grade computer science student at Bursa Technical
+              University. I love experiencing new technologies. I spend the most
+              of my time with activities that improve my current skills and
+              knowledge.
+            </p>
+          </div>
+          <img
+            className="mt-8 mr-16 lg:transition-shadow flex-2 duration-300"
+            src={require("../assets/images/photo_2021-12-29_16-26-58.jpg")}
+            alt="profile photo"
+            height={"200px"}
+            width={"200px"}
+          />
+        </div>
+        <div className="ml-4 mt-8">
+          <u>
+            <h2 className=" font-bold text-2xl mb-6">Education</h2>
+          </u>
+          <div className="mt-4 flex flex-row gap-x-4">
+            <img
+              src={require("../assets/images/btuLogo.jpg")}
+              height={"50px"}
+              width={"50px"}></img>
+            <div>
+              <p className="font-bold text-lg">
+                Bursa Technical University, Computer Engineering
+              </p>
+              <p className="text-gray-500">GPA: 3.42</p>
+            </div>
+          </div>
+        </div>
+        <div className="ml-4 mt-12 mb-16">
+          <u>
+            <h2 className=" font-bold text-2xl mb-6">Experience</h2>
+          </u>
+          <div className="mt-4 flex flex-row gap-x-4">
+            <img
+              src={require("../assets/images/MoskLogo.jpg")}
+              height={"50px"}
+              width={"50px"}></img>
+            <div>
+              <p className="font-bold text-lg">
+                MOSK Bilişim, Intern{" "}
+                <span className="text-white text-sm ml-4 bg-teal-400 pl-2 pr-2 rounded-2xl">
+                  onsite
+                </span>
+              </p>
+              <p className=" text-gray-500 indent-4 w-[35rem]">
+                I have created frontend side of GIS app. I have used leaflet.js,
+                react.js while creating the app
+              </p>
+            </div>
+          </div>
+          <div className="mt-8 flex flex-row gap-x-4">
+            <img
+              src={require("../assets/images/tubitakLogo.png")}
+              height={"50px"}
+              width={"50px"}></img>
+            <div>
+              <p className="font-bold text-lg">
+                Tubitak Project, Scholarship{" "}
+                <span className="text-white text-sm ml-4 bg-red-400 pl-2 pr-2 rounded-2xl">
+                  remote
+                </span>
+              </p>
+              <p className=" text-gray-500 indent-4 w-[35rem]">
+                I am part of the project. My role in the project is creating
+                academic reports, creating academic summarization, searching
+                similar patents. I have been tasked with all those problem and
+                expected to use english
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="ml-2 mt-12 mr-8 mb-16">
+          <u>
+            <h2 className=" font-bold text-2xl mb-6">Technologies</h2>
+          </u>
+
+          <div className="mt-4 flex flex-row gap-x-4">
+            {Object.keys(imgData).map((key, index) =>
+              index < 13 ? (
+                <img src={imgData[key]} height={"30px"} width={"35px"}></img>
+              ) : (
+                ""
+              )
+            )}
+          </div>
+          <div className="mt-4 flex flex-row gap-x-4">
+            {Object.keys(imgData).map((key, index) =>
+              index >= 13 ? (
+                <img src={imgData[key]} height={"30px"} width={"35px"}></img>
+              ) : (
+                ""
+              )
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
