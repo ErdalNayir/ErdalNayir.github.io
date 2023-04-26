3694967d5c2e3343533b95e3946fba5de28c3d63
ErdalNayir <19360859049@ogrenci.btu.edu.tr>
2023-04-26 15:27:39 +0300

    Github Pages Configuration

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
 import Editor from "./modules/editor";
 import Resume from "./modules/resume";
+import ReactGA from "react-ga";
+const TRACKING_ID = "UA-XXXXX-X"; // OUR_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
   return (
